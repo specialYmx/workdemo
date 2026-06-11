a8dc3e4929d9b13288fc2ce5c9eceb6dbc5979dd
yangmingxin <1355628444@qq.com>
2026-06-11 18:13:06 +0800

    111

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,10 +15,16 @@ module.exports = {
     'prettier/prettier': 'error',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'off',
     'vue/multi-word-component-names': 'off',
     'vue/no-v-html': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      { functions: false, classes: true, variables: true, typedefs: true }
+    ],
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
     'vue/no-unused-components': 'off',
